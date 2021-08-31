a4b8b17a00d46750171f8736db36b5e2f27f2a9b
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-08-30 23:13:29 -0400

    Update index.tsx

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -12,7 +12,7 @@ const TOOLS = (i18n: I18n) => [
   {
     id: 1,
     name: 'MEOWSHI',
-    description: i18n._(t`Redenominate xBGSP into MEOWSHI`),
+    description: i18n._(t`Redenominate xBGSP into BANK`),
     href: '/tools/meowshi',
   },
   {
