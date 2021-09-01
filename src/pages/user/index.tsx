52cf5fe290cef4113aa936449f53f367b856c216
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-09-01 10:50:54 -0400

    Update index.tsx

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -63,14 +63,14 @@ export default function Me() {
   return (
     <Container id="user-page" className="py-4 space-y-3 md:py-8 lg:py-12" maxWidth="2xl">
       <Head>
-        <title>My SUSHI | Sushi</title>
+        <title>My BGSP | Big Swap</title>
         <meta key="description" name="description" content="My SUSHI" />
       </Head>
       <div className="p-4 mb-3 space-y-3">
         <Back />
 
         <Typography component="h1" variant="h2" className=" text-high-emphesis">
-          {i18n._(t`My SUSHI`)}
+          {i18n._(t`My BGSP`)}
         </Typography>
       </div>
 
